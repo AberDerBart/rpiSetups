74c0070f1cace901427e70eaeb4ce5ad85998a58
AberDerBart <nonatz@web.de>
2016-11-10 19:19:25 +0000

    faultier: added system service for mpdScheduler

--- faultier/mpdScheduler.sh
+++ faultier/mpdScheduler.sh
@@ -8,3 +8,17 @@ cd /home/pi/mpdScheduler
 sudo apt-get install python-pip
 sudo pip install parse
 sudo pip install python-mpd2
+
+echo "[Unit]
+Description=mpdScheduler
+Requires=mpd.service
+
+[Service]
+ExecStart=/usr/bin/python /home/pi/mpdScheduler/main.py
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/mpdScheduler.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable mpdScheduler.service
+sudo systemctl start mpdScheduler.service
